522e34da2f20f51bad862b2994aed26fb0bc9e45
cheonal <bulewing99@naver.com>
2022-03-11 22:47:51 +0900

    220311 원거리 돌진몹 구현
    11

--- GoldMetal Game/Assets/2Script/Enemy.cs
+++ GoldMetal Game/Assets/2Script/Enemy.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 using UnityEngine.AI;
 public class Enemy : MonoBehaviour
 {
+    public enum Type {A,B,C};
+    public Type enemyType; 
     public int maxhelath;
     public int curhealth;
     public Transform Target;
-    public BoxCollider meleeArea;   
+    public BoxCollider meleeArea;
+    public GameObject Bullet;
+
     public bool isChase;
     public bool isAttack;
 
@@ -53,8 +57,26 @@ public class Enemy : MonoBehaviour
     }
     void Targeting()
     {
-        float targetRadius = 1.5f;
-        float targetRange = 3f;
+        float targetRadius = 0;
+        float targetRange = 0;
+
+        switch (enemyType)
+        {
+            case Type.A:
+                targetRadius = 1.5f;
+                targetRange = 3f;
+                break;
+
+            case Type.B:
+                targetRadius = 1f;
+                targetRange = 12f;
+                break;
+
+            case Type.C:
+                targetRadius = 0.5f;
+                targetRange = 25f;
+                break;
+        }
         RaycastHit[] rayHits =
            Physics.SphereCastAll(transform.position, targetRadius, 
                                  transform.forward, targetRange, 
@@ -73,11 +95,41 @@ public class Enemy : MonoBehaviour
         isAttack = true;
         anim.SetBool("isAttack", true);
 
-        yield return new WaitForSeconds(0.2f);
-        meleeArea.enabled = true;
+        switch (enemyType)
+        {
+            case Type.A:
+                yield return new WaitForSeconds(0.2f);
+                meleeArea.enabled = true;
 
-        yield return new WaitForSeconds(1f);
-        meleeArea.enabled = false;
+                yield return new WaitForSeconds(1f);
+                meleeArea.enabled = false;
+
+                yield return new WaitForSeconds(1f);
+                break;
+
+            case Type.B:
+                yield return new WaitForSeconds(0.1f);
+                rigid.AddForce(transform.forward * 20, ForceMode.Impulse);
+                meleeArea.enabled = true;
+
+                yield return new WaitForSeconds(0.5f);
+                rigid.velocity = Vector3.zero;
+                meleeArea.enabled = false;
+                    
+                yield return new WaitForSeconds(2f);
+                break;
+
+            case Type.C:
+                yield return new WaitForSeconds(0.5f);
+                GameObject instantBullet = Instantiate(Bullet, transform.position, transform.rotation);
+                Rigidbody rigidBullet = instantBullet.GetComponent<Rigidbody>();
+                rigidBullet.velocity = transform.forward * 20;
+
+                yield return new WaitForSeconds(2f);
+                break;
+        }
+
+
 
         isChase = true;
         isAttack = false;
@@ -113,7 +165,7 @@ public class Enemy : MonoBehaviour
 
     public void HitByGrenade(Vector3 exploisonPos)
     {
-        curhealth -= 100;
+        curhealth -= 300;
         Vector3 reactVec = transform.position - exploisonPos;
         StartCoroutine(OnDamage(reactVec, true));
     }
--- GoldMetal Game/Assets/2Script/Player.cs
+++ GoldMetal Game/Assets/2Script/Player.cs
@@ -353,6 +353,10 @@ public class Player : MonoBehaviour
             {
                 Bullet enemyBullet = other.GetComponent<Bullet>();
                 health -= enemyBullet.damage;
+
+                if (other.GetComponent<Rigidbody>() != null)
+                    Destroy(other.gameObject);
+
                 StartCoroutine(OnDamage());
             }
 
